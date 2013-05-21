7ad7da6709351c80a923935280362aebd906b937
Travis Luong <travis@travisluong.com>
2013-05-21 16:45:13 -0700

    testing socket.io

--- web.js
+++ web.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var app = express();
-app.use(express.logger());
-
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
+app.use(express.logger());
 
 redis.set('foo', 'bar');
 
@@ -11,10 +12,18 @@ redis.get('foo', function(err, value) {
 });
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.sendfile(__dirname + '/index.html');
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
+server.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+io.sockets.on('connection', function(client) {
+  console.log('Client connected...');
+
+  client.on('messages', function(data) {
+    console.log(data);
+  });
 });
