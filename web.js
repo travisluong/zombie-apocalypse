26583d3007bb54cb89a1ed25fd1993bb70d74960
Travis Luong <travis@travisluong.com>
2013-05-23 00:17:21 -0700

    implement save player data to redis

--- web.js
+++ web.js
@@ -12,16 +12,16 @@ app.use(express.static(__dirname + '/public'));
 
 redis.set('foo', 'bar');
 
-redis.get('foo', function(err, value) {
+redis.get('foo', function (err, value) {
   console.log('foo is: ' + value);
 });
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
   response.sendfile(__dirname + '/index.html');
 });
 
 var port = process.env.PORT || 5000;
-server.listen(port, function() {
+server.listen(port, function () {
   console.log("Listening on " + port);
 });
 
@@ -31,14 +31,14 @@ io.configure(function () {
   io.set("polling duration", 10);
 });
 
-io.sockets.on('connection', function(client) {
+io.sockets.on('connection', function (client) {
   console.log('Client connected...');
 
   client.set('state', 1);
 
   client.emit('messages', 'Please enter your name.')
 
-  client.on('disconnect', function() {
+  client.on('disconnect', function () {
     console.log('USER DISCONNECTED');
     client.get('nickname', function(err, name) {
       var message = name + " has left the room.";
@@ -46,26 +46,45 @@ io.sockets.on('connection', function(client) {
     });
   });
 
-  client.on('messages', function(data) {
+  client.on('messages', function (data) {
     console.log(data);
 
-    client.get('state', function(err, state) {
+    client.get('state', function (err, state) {
       console.log(state);
-      switch(state) {
+      switch (state) {
         case 1:
-          client.set('nickname', data);
-          client.set('state', 2);
-          var message = data + " has joined the room.";
-          client.broadcast.emit("messages", message);
-          client.emit("messages", message);
-          break;
+        client.set('nickname', data);
+        client.set('state', 2);
+
+        var player = JSON.stringify({nickname: data, hp: 100});
+
+        redis.hset("players", data, player);
+
+        redis.hget("players", data, function (err, reply) {
+          console.log(reply);
+        });
+
+        var message = data + " has joined the room.";
+        client.broadcast.emit("messages", message);
+        client.emit("messages", message);
+        break;
         case 2:
-          client.get('nickname', function(err, name) {
+        client.get('nickname', function (err, name) {
+          split_words = data.split(' ');
+          if (split_words[0] === 'kill') {
+            client.broadcast.emit("messages", name + " attacked " + split_words[1]);
+            client.emit("messages", name + " attacked " + split_words[1]);
+          } else {
             var message = name + ": " + data;
             client.broadcast.emit("messages", message);
             client.emit("messages", message);
+          }
+          redis.hget("players", name, function (err, reply) {
+            var player = JSON.parse(reply);
+            client.emit('messages', '<span class="hp">' + player.hp.toString() + "hp</span>");
           });
-          break;
+        });
+        break;
       }
     });
   });
